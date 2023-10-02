aac101034a8c01bc964816f0840ec4c423ea0432
mn-48 <nazmul.cse48@gmail.com>
2023-10-02 14:53:04 +0600

    daphne and chat app add

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
