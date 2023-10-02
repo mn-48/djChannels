57750d2ffb23e1f405e37e8a3a6d19363a853952
mn-48 <nazmul.cse48@gmail.com>
2023-10-02 14:54:16 +0600

    asgi server setup

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# application = get_asgi_application()
+
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,3 +129,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Daphne
+ASGI_APPLICATION = "mysite.asgi.application"
