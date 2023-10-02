ca586378fa11f6eb3b7380d275980aa158782eda
mn-48 <nazmul.cse48@gmail.com>
2023-10-02 15:33:44 +0600

    project lavel urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("chat/", include("chat.urls")),
     path('admin/', admin.site.urls),
 ]+ static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
