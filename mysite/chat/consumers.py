d51ea99d3b07a673fe8d6a18cd5369900c91cf60
mn-48 <nazmul.cse48@gmail.com>
2023-10-10 17:03:26 +0600

    LongPollConsumerC

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -162,3 +162,38 @@ class BasicHttpConsumer(AsyncHttpConsumer):
         await self.send_response(200, b"Your response bytes", headers=[
             (b"Content-Type", b"text/plain"),
         ])
+
+    
+import json
+from channels.generic.http import AsyncHttpConsumer
+
+class LongPollConsumer(AsyncHttpConsumer):
+    async def handle(self, body):
+        await self.send_headers(headers=[
+            (b"Content-Type", b"application/json"),
+        ])
+        # Headers are only sent after the first body event.
+        # Set "more_body" to tell the interface server to not
+        # finish the response yet:
+        await self.send_body(b"", more_body=True)
+
+    async def chat_message(self, event):
+        # Send JSON and finish the response:
+        await self.send_body(json.dumps(event).encode("utf-8"))
+
+
+
+# from datetime import datetime
+# from channels.generic.http import AsyncHttpConsumer
+
+# class ServerSentEventsConsumer(AsyncHttpConsumer):
+#     async def handle(self, body):
+#         await self.send_headers(headers=[
+#             (b"Cache-Control", b"no-cache"),
+#             (b"Content-Type", b"text/event-stream"),
+#             (b"Transfer-Encoding", b"chunked"),
+#         ])
+#         while True:
+#             payload = "data: %s\n\n" % datetime.now().isoformat()
+#             await self.send_body(payload.encode("utf-8"), more_body=True)
+#             await asyncio.sleep(1)
