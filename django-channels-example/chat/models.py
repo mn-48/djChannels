069370a172d0bad3dc74e282270ded7dc4af1a3e
mn-48 <nazmul.cse48@gmail.com>
2023-10-12 12:37:20 +0600

    django chat example

---
+++ django-channels-example/chat/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=128)
+    online = models.ManyToManyField(to=User, blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+
+    def leave(self, user):
+        self.online.remove(user)
+
+    def __str__(self):
+        return f"{self.name} ({self.get_online_count()})"
+
+class Message(models.Model):
+    User = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.content} [{self.timestamp}]"
